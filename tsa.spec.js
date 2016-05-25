04b0ab1ec24ca1adeb8d27e1472ecacf9cf42664
Shola Abaogun <admin@codedgeek.com>
2016-05-25 15:50:51 +0100

    Update the test to ES6

--- tsa.spec.js
+++ tsa.spec.js
@@ -1,16 +1,16 @@
 describe("validate input from user", function(){
-	it("return a string", function(){
-		var tweets= new FetchTweets('shawlhar');
-		expect(typeof tweets.username).toBe(typeof '');
-	});
-	it("should be greater than 0", function(){
-		var tweets= new FetchTweets('shawlhar');
-		expect(tweets.username.length).not.toBeLessThan(0);
-	});
-	it("return Enter a Username", function(){
-		expect(function(){new FetchTweets('');}).toBe("Enter a Username");
-	});
-	it("return Enter a Username", function(){
-		expect(function(){tweets()}).toBe("Enter a Username");
-	});
+  it("should return a string", function(){
+    let tweets= new FetchTweets('shawlhar');
+    expect(typeof tweets.username).toBe(typeof '');
+  });
+  it("should be greater than 0", function(){
+    let tweets= new FetchTweets('shawlhar');
+    expect(tweets.username.length).not.toBeLessThan(0);
+  });
+  it("should return Enter a Username", function(){
+    expect(function(){new FetchTweets('');}).toBe("Enter a Username");
+  });
+  it("should return Enter a Username", function(){
+    expect(function(){tweets()}).toBe("Enter a Username");
+  });
 });

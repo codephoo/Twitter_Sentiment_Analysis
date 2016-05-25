10ff1596faaa8799f6a277bca1d6746a465af29e
Shola Abaogun <admin@codedgeek.com>
2016-05-25 16:17:00 +0100

    Update the test to ES6

--- tsa.spec.js
+++ tsa.spec.js
@@ -1,16 +1,34 @@
-describe("validate input from user", function(){
-  it("should return a string", function(){
+describe("validate input from user", () =>{
+  it("should return a string", () =>{
     let tweets= new FetchTweets('shawlhar');
     expect(typeof tweets.username).toBe(typeof '');
   });
-  it("should be greater than 0", function(){
+  it("should be greater than 0", () => {
     let tweets= new FetchTweets('shawlhar');
     expect(tweets.username.length).not.toBeLessThan(0);
   });
-  it("should return Enter a Username", function(){
-    expect(function(){new FetchTweets('');}).toBe("Enter a Username");
+  it("should return Enter a Username", () => {
+    expect(()=>{new FetchTweets('');}).toBe("Enter a Username");
   });
-  it("should return Enter a Username", function(){
-    expect(function(){tweets()}).toBe("Enter a Username");
+  it("should return Enter a Username", () =>{
+    expect(()=>{tweets()}).toBe("Enter a Username");
+  });
+});
+describe("connect to Twitter API", () => {
+  it("return a JSON object", () =>{
+    let tweets= new FetchTweets('shawlhar');
+    expect(tweets.getTweets(screen_name)).toBe(typeof {});
+  });
+  it('should return protected tweets',() =>{
+    let tweets= new FetchTweets('shawlhar')
+    expect(tweets.out_of_bound).toBe('Hold up, those are protected tweets.');
+  });
+  it("return Username not found", () => {
+    let tweets= new FetchTweets('Non existing Username')
+    expect(new tweets.getTweets()).toBe("Username not found");
+  });
+  it('return User has no tweets', () =>{
+    let tweets= new FetchTweets('sjsdfds')
+    expect(tweets.getTweets()).toBe("User has no tweets"));
   });
 });
